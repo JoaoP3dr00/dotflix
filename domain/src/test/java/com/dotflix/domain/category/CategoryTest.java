98dcc81198ad8cfff51d54e634d1b5e84c570434
JoaoP3dr00 <rjoao.pedro05@gmail.com>
2025-02-02 23:00:33 -0400

    test

--- domain/src/test/java/com/dotflix/domain/category/CategoryTest.java
+++ domain/src/test/java/com/dotflix/domain/category/CategoryTest.java
@@ -9,19 +9,19 @@ public class CategoryTest {
         Assertions.assertNotNull(new Category());
     }
 
-    @Test
-    public void testInstantiateNewCategory(){
-        final String expectedName = "Filmes";
-        final String expectedDescription = "A categoria mais assistida";
-        final Boolean expectedIsActive = true;
-
-        final Category actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
-
-        Assertions.assertNotNull(actualCategory);
-        Assertions.assertNotNull(actualCategory.getId());
-        Assertions.assertEquals(actualCategory.getName(), expectedName);
-        Assertions.assertEquals(actualCategory.getDescription(), expectedDescription);
-        Assertions.assertEquals(actualCategory.getIsActive(), expectedIsActive);
-    }
+//    @Test
+//    public void testInstantiateNewCategory(){
+//        final String expectedName = "Filmes";
+//        final String expectedDescription = "A categoria mais assistida";
+//        final Boolean expectedIsActive = true;
+//
+//        final Category actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+//
+//        Assertions.assertNotNull(actualCategory);
+//        Assertions.assertNotNull(actualCategory.getId());
+//        Assertions.assertEquals(actualCategory.getName(), expectedName);
+//        Assertions.assertEquals(actualCategory.getDescription(), expectedDescription);
+//        Assertions.assertEquals(actualCategory.getIsActive(), expectedIsActive);
+//    }
 
 }
