9813c1db5b7004087c72cb4cfd4ae1243192d108
JoaoP3dr00 <rjoao.pedro05@gmail.com>
2025-02-02 22:08:31 -0400

    ci: add ci.yaml and create develop branch

--- domain/src/test/java/com/dotflix/domain/category/CategoryTest.java
+++ domain/src/test/java/com/dotflix/domain/category/CategoryTest.java
@@ -8,4 +8,20 @@ public class CategoryTest {
     public void testNewCategory() {
         Assertions.assertNotNull(new Category());
     }
+
+    @Test
+    public void testInstantiateNewCategory(){
+        final String expectedName = "Filmes";
+        final String expectedDescription = "A categoria mais assistida";
+        final Boolean expectedIsActive = true;
+
+        final Category actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        Assertions.assertNotNull(actualCategory);
+        Assertions.assertNotNull(actualCategory.getId());
+        Assertions.assertEquals(actualCategory.getName(), expectedName);
+        Assertions.assertEquals(actualCategory.getDescription(), expectedDescription);
+        Assertions.assertEquals(actualCategory.getIsActive(), expectedIsActive);
+    }
+
 }
