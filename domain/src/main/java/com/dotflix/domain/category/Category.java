0d3f1e2164b3dc33169b6087d499f873f150e613
JoaoP3dr00 <rjoao.pedro05@gmail.com>
2025-02-04 16:06:15 -0400

    feat: add category entity class

--- domain/src/main/java/com/dotflix/domain/category/Category.java
+++ domain/src/main/java/com/dotflix/domain/category/Category.java
@@ -1,5 +1,96 @@
 package com.dotflix.domain.category;
 
+import java.time.Instant;
+import java.util.UUID;
+
 public class Category {
-    public String id;
+    private String id;
+    private String name;
+    private String description;
+    private boolean active;
+    private Instant createdAt;
+    private Instant updatedAt;
+    private Instant deletedAt;
+
+    public Category(){
+
+    }
+
+    private Category(final String id, final String name, final String description, final boolean active, final Instant createdAt, final Instant updatedAt, final Instant deletedAt) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.active = active;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.deletedAt = deletedAt;
+    }
+
+    /**
+     * Factory Method that
+     * @param name
+     * @param description
+     * @param active
+     * @return
+     */
+    public static Category newCategory(final String name, final String description, final boolean active){
+        final String id = UUID.randomUUID().toString();
+        return new Category(id, name, description, active, Instant.now(), Instant.now(), Instant.now());
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public Instant getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public void setDeletedAt(Instant deletedAt) {
+        this.deletedAt = deletedAt;
+    }
 }
