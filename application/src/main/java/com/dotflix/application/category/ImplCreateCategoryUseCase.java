ee90523b3aa3c7398a19f1a81aeb7572552c3bc4
JoaoP3dr00 <rjoao.pedro05@gmail.com>
2025-02-05 21:09:31 -0400

    fix: resolve bugs

--- application/src/main/java/com/dotflix/application/category/ImplCreateCategoryUseCase.java
+++ application/src/main/java/com/dotflix/application/category/ImplCreateCategoryUseCase.java
@@ -1,7 +1,15 @@
 package com.dotflix.application.category;
 
+import com.dotflix.domain.category.CategoryGateway;
+
+import java.util.Objects;
+
 public class ImplCreateCategoryUseCase extends CreateCategoryUseCase{
-    private final CategoryGat categoryGateway;
+    private final CategoryGateway categoryGateway;
+
+    public ImplCreateCategoryUseCase(final CategoryGateway categoryGateway){
+        this.categoryGateway = Objects.requireNonNull(categoryGateway);
+    }
 
     @Override
     public CreateCategoryOutput execute(final CreateCategoryCommand createCategoryCommand) {
