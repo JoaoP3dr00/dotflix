282ded5187aa81f478ff9f11771dcd491e5038ab
JoaoP3dr00 <rjoao.pedro05@gmail.com>
2025-02-07 20:25:34 -0400

    chore: add MainTest.java to git.ignore

--- infrastructure/src/test/java/com/dotflix/infrastructure/MainTest.java
+++ infrastructure/src/test/java/com/dotflix/infrastructure/MainTest.java
@@ -1,7 +1,9 @@
 package com.dotflix.infrastructure;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.AbstractEnvironment;
+import org.springframework.test.context.ActiveProfiles;
 
 public class MainTest {
     @Test
